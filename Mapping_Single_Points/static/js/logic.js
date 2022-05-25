20a6c8b0347a2b5e34b2a0d6ce17bc2bded1eeba
mustafaikizler <98247252+mustafaikizler@users.noreply.github.com>
2022-05-25 10:16:55 -0400

    Creating circle radius on a dark map layer

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,16 +10,27 @@ let map = L.map("mapid",{
 
     center:[
 
-        40.7, -94.5
+        34.0522, -118.2437
 
     ],
 
-    zoom:4
+    zoom:14
 });
 
-// we create the tile layer that will be the background of our map.
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Add a circle to the map for Los Angeles California
+let circle = L.circleMarker([34.0522, -118.2437],{
+    radius:300,
+    color:"black",
+    fillcolor:'#ffffa1'
+}).addTo(map)
+
+
+// Add a marker to the map for LOs Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// we create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -28,3 +39,4 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // then we add our "graymap" tile layer to the map.
 
 streets.addTo(map);
+
