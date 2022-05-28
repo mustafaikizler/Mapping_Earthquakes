c7bd7c91c854d3501b02c32095262bce4eea42a6
mustafaikizler <98247252+mustafaikizler@users.noreply.github.com>
2022-05-27 21:57:16 -0400

    adding function to style

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -41,7 +41,42 @@ L.control.layers(baseMap).addTo(map)
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data){
     console.log(data);
 // CREATING A GEOJSON DATA WITH THE RETRIEVED DATA
-L.geoJSON(data).addTo(map)
+L.geoJSON(data,{
+    // we turn each feature into a circleMarker on the map.
+    pointToLayer:function(feature,latlng){
+        console.log(data);
+        return L.circleMarker(latlng);
+    },
+    style:styleInfo
+}).addTo(map)
     
 });
+   
+        
+           // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+//   this function determines the radius of the earthquake marker based on its magnitude, Earthquakes with a magnitude of 0 will be plotted with a radius of 1
+function getRadius(magnitude){
+    if (magnitude === 0){
+        return 1;
+    }
+    return magnitude * 4
+}
     
+
+ 
+    
+ 
